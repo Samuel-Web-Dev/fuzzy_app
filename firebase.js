123beaf83d67f7e324b226e54a0c94b9d51e0bb4
adeagbo <adeagbojosiah1@gmail.com>
2023-07-11 12:46:16 +0100

    first commit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC9bYZno33reps-a0iorttht_OMjpsjy2o",
+    authDomain: "login-test-4f7cd.firebaseapp.com",
+    projectId: "login-test-4f7cd",
+    storageBucket: "login-test-4f7cd.appspot.com",
+    messagingSenderId: "767596281646",
+    appId: "1:767596281646:web:5e1de8e8a80090021584bf",  
+    measurementId: "G-6EF9ND3KRX"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
